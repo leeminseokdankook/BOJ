f180e78584e88f58e966b2f3d0cf03f70994e5d0
leeminseokdankook <32133660+leeminseokdankook@users.noreply.github.com>
2021-05-17 15:00:14 +0900

    Create 16120.cpp

---
+++ 16120.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    string ppap;
+    stack<int> p_stack;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cin>>ppap;
+    for(int i=0;i<ppap.size();)
+    {
+        if(ppap[i]=='A')
+        {
+            if(i+1<ppap.size()&&ppap[i+1]=='P'&&p_stack.size()>=2)
+            {
+                p_stack.pop();
+                i=i+2;
+            }
+            else
+            {
+                cout<<"NP";
+                return 0;
+            }
+        }
+        else
+        {
+            p_stack.push(1);
+            i=i+1;
+        }
+    }
+    if(p_stack.size()==1)
+    {
+        cout<<"PPAP";
+    }
+    else
+    {
+        cout<<"NP";
+    }
+    return 0;
+}
